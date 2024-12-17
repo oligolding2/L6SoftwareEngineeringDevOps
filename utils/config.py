3bd1ad81441e956f4163a580672c2458c13287cd
oligolding2 <oliverg@blueyonder.co.uk>
2024-12-17 17:19:07 +0000

    Amended file references

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     FLASK_ENV = os.environ.get('FLASK_ENV','production')
     SECRET_KEY=os.environ.get('SECRET_KEY',os.urandom(24))
-    DATABASE_URL= os.environ.get('DATABASE_URL','static/database.db')
+    DATABASE_URL= os.environ.get('DATABASE_URL','static/db/database.db')
     SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE',True)
     SESSION_COOKIE_HTTPONLY = os.environ.get('SESSION_COOKIE_HTTPONLY',True)
     SESSION_COOKIE_SAMESITE = os.environ.get('SESSION_COOKIE_SAMESITE','Lax')
